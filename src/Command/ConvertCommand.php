057207de3e8b7351a8f07e1cd918a1f735656d47
Rich Lott / Artful Robot <forums@artfulrobot.uk>
2021-01-15 17:55:54 +0000

    Add progress bars for big imports

--- src/Command/ConvertCommand.php
+++ src/Command/ConvertCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\ProgressBar;
 
 class ConvertCommand extends Command
 {
@@ -18,7 +19,7 @@ class ConvertCommand extends Command
          ->addOption('input', 'i', InputOption::VALUE_REQUIRED, 'input CSV filename')
          // ->addOption('output', 'o', InputOption::VALUE_REQUIRED, 'output SQL filename')
          ->addOption('drop', 'd', InputOption::VALUE_NONE, 'Include DROP TABLE? (default no, and use CREATE TABLE IF NOT EXISTS)')
-         ->addOption('schema', 's', InputOption::VALUE_REQUIRED, 'Just the schema, no INSERTs')
+         ->addOption('schema', 's', InputOption::VALUE_NONE, 'Just the schema, no INSERTs')
          ->addOption('csv-read-buffer', 'b', InputOption::VALUE_REQUIRED, 'Buffer size in kB. Each line of CSV must be shorter than this. Default 10', 10)
          ->addOption('max-command-length', 'm', InputOption::VALUE_REQUIRED, 'Max length of INSERT SQL command in kB. Default 10.', 10)
        ;
@@ -33,10 +34,23 @@ class ConvertCommand extends Command
        return 1;
     }
     if (!is_readable($csvFilename)) {
-       $output->writeln("<error>Error: input '$csvFilename' not readable</error>");
+       $output->writeln("<error>-- Error: input '$csvFilename' not readable</error>");
        return 1;
     }
-    $parser = CsvParser::createFromFile($csvFilename, $input->getOption('csv-read-buffer') * 1024);
+    try {
+      $parser = CsvParser::createFromFile($csvFilename, $input->getOption('csv-read-buffer') * 1024);
+    }
+    catch (\Exception $e) {
+      $output->writeln("<error>-- Error: input '$csvFilename': " . $e->getMessage() . "</error>");
+      return 1;
+    }
+    if ($parser->count() == 0) {
+      $output->writeln("<comment>-- Ignored file with no row data: $csvFilename</comment>");
+      return 1;
+    }
+
+    $isBigFile = ($parser->count() > 100);
+    ProgressBar::setFormatDefinition('custom', '  <info>%message%</info> %percent% %bar% Est %remaining%');
 
     $tableName = preg_replace( '/[^a-zA-Z0-9_]+/', '_',
       preg_replace('@^(?:.*/)?([^/]+)\.csv$@', '$1', $csvFilename));
@@ -47,28 +61,43 @@ class ConvertCommand extends Command
     foreach ($parser->getHeaders() as $colname) {
       $cols[$colname] = [
         'name' => trim(preg_replace('/[^a-zA-Z0-9]+/', '_', $colname)),
+        // A column type is true until a non-valid value is encountered.
         'date' => TRUE,
         'datetime' => TRUE,
-        'empty' => TRUE,
         'unsigned_int' => TRUE,
         'signed_int' => TRUE,
         'float' => TRUE,
+
+        // Nulls are allowed as soon as an empty cell is found.
+        'empty' => FALSE,
         'maxlength' => 0,
         'maxmblength' => 0,
         'maxint' => 0,
       ];
     }
 
+    // 2x because we scan and then we generate the INSERTS
+    if ($isBigFile) {
+      $progressBar = new ProgressBar($output, count($cols));
+      $progressBar->setFormat('custom');
+      $progressBar->setMessage("Scanning schema.");
+      $progressBar->start();
+    }
+
     foreach (array_keys($cols) as $colname) {
+      $t = &$cols[$colname];
+
       foreach ($parser as $row) {
         $val = $row->$colname;
-        $t = &$cols[$colname];
+        // Rule out certain types if the value is not valid for them.
         $t['unsigned_int'] &= preg_match('/^\d+$/', (string) $val);
         $t['signed_int'] &= preg_match('/^-\d+$/', $val);
         $t['float'] &= preg_match('/^-?\d\d*(\.\d+)$/', $val);
-        $t['empty'] &= $val === '';
         $t['date'] &= preg_match('/^\d\d\d\d-\d\d-\d\d$/', $val);
         $t['datetime'] &= preg_match('/^\d\d\d\d-\d\d-\d\d[T ]\d\d:\d\d:\d\d$/', $val);
+
+        // Are there empty values? (Note here, empty means zero-length string.)
+        $t['empty'] |= $val === '';
 
         // @todo timezones
         $t['maxmblength'] = max(mb_strlen($val), $t['maxlength']);
@@ -90,7 +119,7 @@ class ConvertCommand extends Command
         $type = $t['unique'][0];
       }
       elseif ($c > 1) {
-        throw new \RuntimeException("Row " . $row->key() . " col '" . $colname . "' could be "
+        throw new \RuntimeException("Row " . $parser->key() . " col '" . $colname . "' could be "
           . implode(' or ', $t['unique']));
       }
 
@@ -106,9 +135,9 @@ class ConvertCommand extends Command
           // allow 10% more than the max chars so far.
           $t['def'] = 'VARCHAR(' . ((int) (1.10 * $t['maxmblength'])) . ')';
         }
-        if (! $t['empty']) {
-          $t['def'] .= ' NOT NULL DEFAULT ""';
-        }
+        // We don't know that we need to differentiate between zero length string and NULL;
+        // the INSERTS will be zls, so call this column NOT NULL.
+        $t['def'] .= ' NOT NULL DEFAULT ""';
       }
       elseif ($type === 'unsigned_int') {
         if ($t['maxint'] <256) {
@@ -153,7 +182,7 @@ class ConvertCommand extends Command
         }
       }
       else {
-        throw new \RuntimeException("Row " . $row->key() . " col '" . $colname . "' unexpected type '$type'");
+        throw new \RuntimeException("Row " . $parser->key() . " col '" . $colname . "' unexpected type '$type'");
       }
       $cols[$colname]['type'] = $type;
 
@@ -161,6 +190,9 @@ class ConvertCommand extends Command
       if (strtolower($t['name']) === 'id') {
         $schemaCols[$t['name']] .= ' PRIMARY KEY';
         $foundID = $t['name'];
+      }
+      if (isset($progressBar)) {
+        $progressBar->advance();
       }
     }
 
@@ -184,6 +216,11 @@ class ConvertCommand extends Command
 
     $output->writeln("<info>$schemaSQL</info>");
 
+    if (isset($progressBar)) {
+      $progressBar->finish();
+      $progressBar->clear();
+    }
+
     if ($input->getOption('schema')) {
       return 0;
     }
@@ -198,6 +235,12 @@ class ConvertCommand extends Command
     $maxValuesLength = $maxCommandLength - strlen($insert);
     $command = $insert;
     $sep = '';
+    if (isset($progressBar)) {
+      $progressBar = new ProgressBar($output, $parser->count());
+      $progressBar->setFormat('custom');
+      $progressBar->setMessage("Writing INSERTS");
+      $progressBar->start();
+    }
     foreach ($parser as $row) {
       $data = [];
       foreach ($cols as $header=>$col) {
@@ -224,9 +267,15 @@ class ConvertCommand extends Command
       }
       $command .= $sep . $data;
       $sep = ',';
+
+      if (isset($progressBar)) {
+        $progressBar->advance();
+      }
     }
     $command .= ";\n";
     $output->writeln($command);
+    $progressBar->finish();
+    $progressBar->clear();
 
     return 0;
   }
